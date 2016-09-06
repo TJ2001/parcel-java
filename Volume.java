e0f7d4dc442a79c34f838db3a4ad8c092aa6c260
Nhat Hoang and Tim Jung <nhat+tim@>
2016-09-06 14:12:15 -0700

    refactor code

--- Volume.java
+++ Volume.java
@@ -2,27 +2,27 @@ import java.io.Console;
 
 public class Volume{
   public static void main(String[] args){
-    Console myConsole = System.console();
 
-    System.out.println("Please enter parcel height.");
-    Integer height = Integer.parseInt(myConsole.readLine());
-    System.out.println("please enter parcel width.");
-    Integer width = Integer.parseInt(myConsole.readLine());
-    System.out.println("Please enter parcel length.");
-    Integer length = Integer.parseInt(myConsole.readLine());
+    Integer height = askForInputs("height");
+    Integer width = askForInputs("width");
+    Integer length = askForInputs("length");
 
     Parcel inputParcel = new Parcel(height, width, length);
     Integer dimensions = inputParcel.getVolume();
 
-    System.out.println("Please enter parcel weight");
-    Integer weight = Integer.parseInt(myConsole.readLine());
-    System.out.println("Please enter speed of delivery: 1 day, 3 days, 5 days.");
-    Integer days = Integer.parseInt(myConsole.readLine());
-    System.out.println("Please enter the parcel distance travel.");
-    Integer distance = Integer.parseInt(myConsole.readLine());
+    Integer weight =  askForInputs("weight");
+    Integer days = askForInputs("days till delivery: 1 day, 3 days, 5 days.");
+    Integer distance = askForInputs("distance");
 
     ShippingDetails packageDetails = new ShippingDetails(weight, days, distance, dimensions);
 
     packageDetails.getCostToShip();
+  }
+
+  public static Integer askForInputs(String details) {
+    Console myConsole = System.console();
+
+    System.out.println("Please enter parcel " + details + ".");
+    return Integer.parseInt(myConsole.readLine());
   }
 }
